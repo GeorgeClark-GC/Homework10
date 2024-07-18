9915dbd07bc1252da1f03ade67182364cb246465
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-18 23:11:20 +0300

    Corrections v1

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,15 @@ public class Main {
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
         System.out.println("Example2");
-        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчёта - " + fullName.toUpperCase());
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчёта — " + fullName.toUpperCase());
         System.out.println("Example1");
         String firstNameNew = "Семён";
         String middleNameNew = "Семёнович";
         String lastNameNew = "Иванов";
         String fullNameNew = lastNameNew + " " + firstNameNew + " " + middleNameNew;
         String correctFullName = fullNameNew.replace("ё", "е");
-        System.out.println("Ф.И.О. сотрудника - " + correctFullName);
+        System.out.println("Ф.И.О. сотрудника — " + correctFullName);
     }
 }
